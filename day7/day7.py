21063cd9152fe7fe54d06afe812c1b0b1c3dbef7
Daniel <daniale@student.chalmers.se>
2021-12-07 13:05:20 +0100

    Solve part 2 for day 7

--- day7/day7.py
+++ day7/day7.py
@@ -14,4 +14,17 @@ def solve_part1(example=False):
     return alignment_fuel_spent
 
 
+def solve_part2(example=False):
+    data = parse_input(example)
+    least_fuel_position = int(statistics.mean(data))
+
+    def alignment_cost(from_pos):
+        position_diff = abs(from_pos - least_fuel_position)
+        return (position_diff * (position_diff + 1)) / 2
+
+    alignment_fuel_spent = sum(alignment_cost(d) for d in data)
+    return int(alignment_fuel_spent)
+
+
 print(solve_part1(False))
+print(solve_part2(False))
