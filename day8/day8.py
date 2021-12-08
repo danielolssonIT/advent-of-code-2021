68235125605923e3cf9abc3b59155f91c438da01
Daniel <daniale@student.chalmers.se>
2021-12-08 20:48:43 +0100

    Solve part 2 for day 8

--- day8/day8.py
+++ day8/day8.py
@@ -24,6 +24,10 @@ def parse_input(example=False):
     return signal_patterns_output_pairs
 
 
+def diff_chars(s1, s2):
+    return ''.join(set(s1).symmetric_difference(set(s2)))
+
+
 def solve_part1(example=False):
     data = parse_input(example)
     unique_segment_count = 0
@@ -33,4 +37,52 @@ def solve_part1(example=False):
     return unique_segment_count
 
 
+def solve_part2(example=False):
+    data = parse_input(example)
+    output_value_sum = 0
+    for signal_patterns, output_values in data:
+        # Top, topleft, topright, middle, bottomleft, bottomright, bottom
+        keys = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+        signal_wiring_segment = dict((el, '') for el in keys)
+
+        sorted_signal_patterns = sorted(signal_patterns, key=len)
+        topleft_middle = ['b', 'd']
+        topright_bottomright = ['c', 'f']
+        bottomleft_bottom = ['e', 'g']
+
+        signal_wiring_segment['a'] = diff_chars(sorted_signal_patterns[1], sorted_signal_patterns[0])
+        for i in topright_bottomright:
+            signal_wiring_segment[i] = sorted_signal_patterns[0]
+
+        rem_keys = diff_chars(sorted_signal_patterns[0], sorted_signal_patterns[2])
+        for i in topleft_middle:
+            signal_wiring_segment[i] = ''.join(rem_keys)
+
+        locked_eight = ''.join(signal_wiring_segment[i] for i in ['a', 'b', 'c'])
+        rem_keys = diff_chars(locked_eight, sorted_signal_patterns[-1])
+        for i in bottomleft_bottom:
+            signal_wiring_segment[i] = rem_keys
+
+        rem_segments = [diff_chars(sorted_signal_patterns[-1], sorted_signal_patterns[i]) for i in range(6, 9)]
+        rem_segments_keys = ['c', 'd', 'e']
+        related_keys = {'c': 'f', 'd': 'b', 'e': 'g'}
+        for rem in rem_segments:
+            for key in rem_segments_keys:
+                if rem in signal_wiring_segment[key]:
+                    signal_wiring_segment[key] = rem
+                    related_key = related_keys[key]
+                    signal_wiring_segment[related_key] = signal_wiring_segment[related_key].replace(rem, "")
+                    break
+
+        digit = ""
+        swapped_wiring_segment = dict((v, k) for k, v in signal_wiring_segment.items())
+        for val in output_values:
+            rerouted_val = ''.join(swapped_wiring_segment[c] for c in val)
+            digit += str(segment_values[''.join(sorted(rerouted_val))])
+        output_value_sum += int(digit)
+
+    return output_value_sum
+
+
 print(solve_part1())
+print(solve_part2())
