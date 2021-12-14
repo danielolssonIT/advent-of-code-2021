9597584581e6f408785262de5969ddb26d10a599
Daniel <daniale@student.chalmers.se>
2021-12-14 16:59:45 +0100

    Solve part 2 for day 14

--- day14/day14.py
+++ day14/day14.py
@@ -1,30 +1,42 @@
 import collections
 
+
 def parse_input(example=False):
     filename = "example.txt" if example else "day14.txt"
     with open(filename) as f:
         template, pair_insertions_str = f.read().split("\n\n", 1)
         pair_insertions = (line.split(" -> ") for line in pair_insertions_str.split("\n"))
         rules = dict((a, b) for a, b in pair_insertions)
-        return list(template), rules
+        return template, rules
 
 
-def solve_part1(example=False):
+def solve(example=False, steps=1):
     template, rules = parse_input(example)
-    for step in range(10):
-        elems_to_insert = []
-        for i in range(1, len(template)):
-            pair = template[i-1] + template[i]
-            elem = rules.get(pair, "")
-            if elem:
-                elems_to_insert.append((i, elem))
-        for j, elem in enumerate(elems_to_insert):
-            insert_index = elem[0]
-            c = elem[1]
-            template.insert(insert_index + j, c)
-    element_count = collections.Counter(template).most_common()
-    return element_count[0][1] - element_count[-1][1]
+    pair_count = collections.defaultdict(int)
+    element_count = collections.defaultdict(int)
+    for i in range(1, len(template)):
+        pair_count[template[i - 1:i + 1]] += 1
+    for c in template:
+        element_count[c] += 1
+
+    for step in range(steps):
+        pairs_to_add = collections.defaultdict(int)
+        for pair, count in pair_count.items():
+            if count == 0:
+                continue
+            pair_count[pair] = 0
+            inserted_element = rules[pair]
+            element_count[inserted_element] += count
+            pair1 = pair[0] + inserted_element
+            pair2 = inserted_element + pair[1]
+            for p in [pair1, pair2]:
+                pairs_to_add[p] += count
+        for k, v in pairs_to_add.items():
+            pair_count[k] += v
+
+    sorted_elem_counts = sorted(element_count.values())
+    return sorted_elem_counts[-1] - sorted_elem_counts[0]
 
 
-
-print(solve_part1())
+print(solve(steps=10))
+print(solve(steps=40))
