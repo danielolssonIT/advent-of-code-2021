a20ec7f46f59a4daa17ee2269bd057a8af1214b7
Daniel <daniale@student.chalmers.se>
2021-12-15 11:58:08 +0100

    Solve part 2 for day 15

--- day15/day15.py
+++ day15/day15.py
@@ -1,5 +1,6 @@
 import networkx as nx
 
+
 def in_bounds(row, col, max_row, max_col):
     return 0 <= row <= max_row and 0 <= col <= max_col
 
@@ -9,11 +10,8 @@ def adjacent_positions(row, col, max_row, max_col):
     return [(i, j) for i, j in adj_pos if in_bounds(i, j, max_row, max_col)]
 
 
-def parse_input(example=False):
-    filename = "example.txt" if example else "day15.txt"
+def create_graph(grid):
     graph = nx.DiGraph()
-    with open(filename) as f:
-        grid = [line.strip() for line in f.readlines()]
     max_rows = len(grid) - 1
     max_cols = len(grid[0]) - 1
     for row, line in enumerate(grid):
@@ -25,9 +23,39 @@ def parse_input(example=False):
     return graph, max_rows, max_cols
 
 
-def solve_part1(example=False):
-    graph, rows, cols = parse_input(example)
-    return nx.dijkstra_path_length(graph, (0, 0),  (rows, cols))
+def parse_input(example=False, full_map=False):
+    filename = "example.txt" if example else "day15.txt"
+    with open(filename) as f:
+        grid = [line.strip() for line in f.readlines()]
+    if full_map:
+        extend_grid_full_map(grid)
+    return create_graph(grid)
 
 
-print(solve_part1())
+def inc_risk(risk, term):
+    n = int(risk) + term
+    return n if n <= 9 else n % 9
+
+
+def inc_row(row, term):
+    return ''.join(str(inc_risk(value, term)) for value in row)
+
+
+def extend_grid_full_map(grid):
+    for r, line in enumerate(grid):
+        for term in range(1, 5):
+            grid[r] += inc_row(line, term)
+
+    row_count_before = len(grid)
+    for term in range(1, 5):
+        for i in range(row_count_before):
+            grid.append(inc_row(grid[i], term))
+
+
+def solve(example=False, full_map=False):
+    graph, rows, cols = parse_input(example, full_map)
+    return nx.dijkstra_path_length(graph, (0, 0), (rows, cols))
+
+
+print(solve())
+print(solve(full_map=True))
