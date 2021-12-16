0b416e9569a61a8ef5b3a5b90ee5abcd182ceb3a
Daniel <daniale@student.chalmers.se>
2021-12-16 21:39:13 +0100

    Solve part 2 for day 16

--- day16/day16.py
+++ day16/day16.py
@@ -1,4 +1,14 @@
-import re
+import functools
+
+type_id_operator = {
+    0: lambda a, b: a + b,
+    1: lambda a, b: a * b,
+    2: lambda a, b: min(a, b),
+    3: lambda a, b: max(a, b),
+    5: lambda a, b: int(a > b),
+    6: lambda a, b: int(a < b),
+    7: lambda a, b: int(a == b)
+}
 
 
 def parse_input(example=False):
@@ -16,20 +26,51 @@ def packet_version_sum(message):
     version = int(message[:3], 2)
     type_id = int(message[3:6], 2)
     if type_id == 4:
-        #nearest_four_multiple = (len(data) + 3) & ~0x03
         i = 6
         while message[i] != "0":
             i += 5
-        return version + packet_version_sum(message[i+5:])
+        return version + packet_version_sum(message[i + 11:])
 
     length_type_id = int(message[6])
     if length_type_id == 0:
-        #sub_packet_length_bits = int(message[7:22], 2)
         return version + packet_version_sum(message[22:])
-    else:
-        #sub_packet_count = int(message[7:18], 2)
-        return version + packet_version_sum(message[18:])
+    return version + packet_version_sum(message[18:])
 
+
+def literal_packet_value(data, index):
+    val = ""
+    i = index + 6
+    while data[i] != "0":
+        val += data[i + 1:i + 5]
+        i += 5
+    val += data[i + 1:i + 5]
+    return int(val, 2), i + 5
+
+
+def evaluate_packet(data, index):
+    if "1" not in data:
+        return 0, 0
+
+    type_id = int(data[index + 3:index + 6], 2)
+    if type_id == 4:
+        return literal_packet_value(data, index)
+
+    subpacket_values = []
+    length_type_id = int(data[index + 6], 2)
+    if length_type_id == 0:
+        subpacket_bits = int(data[index + 7:index + 22], 2)
+        next_index = index + 22
+        while next_index < index + 22 + subpacket_bits:
+            val, next_index = evaluate_packet(data, next_index)
+            subpacket_values.append(val)
+    else:
+        subpacket_count = int(data[index + 7:index + 18], 2)
+        next_index = index + 18
+        for i in range(subpacket_count):
+            val, next_index = evaluate_packet(data, next_index)
+            subpacket_values.append(val)
+  
+    return functools.reduce(type_id_operator[type_id], subpacket_values), next_index
 
 
 def solve(example=False):
@@ -37,7 +78,10 @@ def solve(example=False):
     return packet_version_sum(bin_message)
 
 
+def solve_part2(example=False):
+    bin_message = parse_input(example)
+    return evaluate_packet(bin_message, 0)
 
 
 print(solve())
-#print(packet_version_sum("00111000000000000110111101000101001010010001001000000000")) # = 9
+print(solve_part2()[0])
