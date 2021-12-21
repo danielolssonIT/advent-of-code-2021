a0edc05625eedca584821964c98c1a521a45926e
Daniel <daniale@student.chalmers.se>
2021-12-21 17:53:34 +0100

    Solve part 2 for day 21

--- day21/day21.py
+++ day21/day21.py
@@ -1,3 +1,7 @@
+import functools
+import itertools
+
+
 def parse_input(example=False):
     filename = "example.txt" if example else "day21.txt"
     with open(filename) as f:
@@ -31,4 +35,33 @@ def solve_part1(example=False):
     return min(score) * die_rolls
 
 
-print(solve_part1())
+@functools.cache
+def wins(start_positions, score, turn):
+    if score[0] >= 21:
+        return 1, 0
+    if score[1] >= 21:
+        return 0, 1
+
+    new_turn = 0 if turn == 1 else 1
+    total_p1_wins = 0
+    total_p2_wins = 0
+    for die_values in itertools.product(range(1, 4), repeat=3):
+        new_pos = list(start_positions)
+        new_score = list(score)
+
+        new_pos[turn] = reset(start_positions[turn] + sum(die_values), 10)
+        new_score[turn] += new_pos[turn]
+
+        p1_wins, p2_wins = wins(tuple(new_pos), tuple(new_score), new_turn)
+        total_p1_wins += p1_wins
+        total_p2_wins += p2_wins
+
+    return total_p1_wins, total_p2_wins
+
+
+def solve_part2(example=False):
+    return wins(parse_input(example), (0, 0), 0)
+
+
+# print(solve_part1())
+print(solve_part2())
