3bb5f5da93a8309248777521b25171a31a17245c
danielolssonIT <31478577+danielolssonIT@users.noreply.github.com>
2021-12-27 14:12:58 +0100

    Solve part 2 for day 22

--- day22/day22.py
+++ day22/day22.py
@@ -1,4 +1,5 @@
 import re
+from collections import Counter
 
 
 def parse_input(example):
@@ -29,4 +30,27 @@ def solve_part1(example=False):
     return len(cuboids)
 
 
-print(solve_part1())
+def solve_part2(example=False):
+    steps = parse_input(example)
+    cuboids = Counter()
+    for cmd, x1, x2, y1, y2, z1, z2 in steps:
+        update = Counter()
+        sign = 1 if cmd == "on" else -1
+        for (ex1, ex2, ey1, ey2, ez1, ez2), count in cuboids.items():
+            interval_x1, interval_x2 = max(x1, ex1), min(x2, ex2)
+            interval_y1, interval_y2 = max(y1, ey1), min(y2, ey2)
+            interval_z1, interval_z2 = max(z1, ez1), min(z2, ez2)
+            if interval_x1 <= interval_x2 and interval_y1 <= interval_y2 and interval_z1 <= interval_z2:
+                update[
+                    (interval_x1, interval_x2, interval_y1, interval_y2, interval_z1, interval_z2)
+                ] -= count
+        if sign == 1:
+            update[(x1, x2, y1, y2, z1, z2)] += sign
+        cuboids.update(update)
+    return sum(
+        (x2 - x1 + 1) * (y2 - y1 + 1) * (z2 - z1 + 1) * sign for (x1, x2, y1, y2, z1, z2), sign in cuboids.items())
+
+
+# print(solve_part1())
+print(solve_part2())
+# print(overlap_cuboids(a1, a2, b1, b2))
